0ff7d5e22ef96cf37f69f6cdb8347b7f8fd58af9
ბესო ბანცაძე < >
2016-06-10 21:31:13 +0400

    Eight Queens Puzzle - backtracking method

---
+++ js/events.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //
+    var solveButton = document.getElementsByName("solve");
+    solveButton[0].addEventListener("click", function () {
+        calculate();
+    });
+});
